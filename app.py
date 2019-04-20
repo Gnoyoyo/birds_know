de79f40b138cfb5254b48a4b137553ac21632075
vorabhol <max003003003@gmail.com>
2019-04-20 21:57:04 +0700

    Add text converter

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, url_for , Response, json, request, url_for
+from flask import *
 app = Flask(__name__)
 
 @app.route('/hello', methods = ['GET'])
@@ -9,18 +9,16 @@ def api_hello():
 
             }
     js = json.dumps(data)
-
     resp = Response(js, status=200, mimetype='application/json')
-
     return resp
 
 @app.route('/', methods = ['GET'])
-def api_wellcome():
-    return "Hello This is a bird know project"
+def index():
+    return send_from_directory("./www", 'index.html')
+
 
 @app.route('/chat', methods = ['POST'])
 def upload_file():
-    print(request.files)
     file = request.files['file']
     file.save("./upload/chat.txt")
     return "success"
